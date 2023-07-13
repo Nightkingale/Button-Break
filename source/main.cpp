313dff7bf4b3df7d5c34141de1262d6b7979d85b
Nightkingale <63483138+Nightkingale@users.noreply.github.com>
2023-07-13 15:39:21 -0600

    Make variable names consistent

--- source/main.cpp
+++ source/main.cpp
@@ -8,51 +8,51 @@
 #include <whb/proc.h>
 
 // Random variables for the game.
-int input_count = 0;
-int current_color = 0;
-bool touch_held_down = false;
+int buttonPressCount = 0;
+int activeColorIndex = 0;
+bool isTouchScreenHeld = false;
 
-void count()
+void updateButtonPressCount()
 {
-    input_count += 1;
-    if (input_count == 1)
+    buttonPressCount += 1;
+    if (buttonPressCount == 1)
     {
         // The very first button is pressed, grammar matters.
-        WHBLogPrintf("You mashed %d button!", input_count);
+        WHBLogPrintf("You mashed %d button!", buttonPressCount);
         WHBLogPrint("----------------------------------------");
     }
     else
     {
         // Anything that isn't the first button press.
-        WHBLogPrintf("You mashed %d buttons!", input_count);
+        WHBLogPrintf("You mashed %d buttons!", buttonPressCount);
         WHBLogPrint("----------------------------------------");
     }
-    if (input_count % 100 == 0)
+    if (buttonPressCount % 100 == 0)
     {
         // Small vibration every hundred presses.
         uint8_t pattern[15];
         memset(pattern, 0x99, 15);
         VPADControlMotor(VPAD_CHAN_0, pattern, 60);
         // Change the color from red, to blue, etc.
-        if (current_color == 0)
+        if (activeColorIndex == 0)
         {
             WHBLogConsoleSetColor(0x99000000);
-            current_color = 1;
+            activeColorIndex = 1;
         }
-        else if (current_color == 1)
+        else if (activeColorIndex == 1)
         {
             WHBLogConsoleSetColor(0x00990000);
-            current_color = 2;
+            activeColorIndex = 2;
         }
-        else if (current_color == 2)
+        else if (activeColorIndex == 2)
         {
             WHBLogConsoleSetColor(0x00009900);
-            current_color = 3;
+            activeColorIndex = 3;
         }
-        else if (current_color == 3)
+        else if (activeColorIndex == 3)
         {
             WHBLogConsoleSetColor(0x99000000);
-            current_color = 1;
+            activeColorIndex = 1;
         }
     }
 }
@@ -80,116 +80,116 @@ int main()
         if (input.tpNormal.touched != 0)
         {
             // Check if the screen is pressed on.
-            if (touch_held_down == false)
+            if (isTouchScreenHeld == false)
             {
                 // Change the color from red, to blue, etc.
-                touch_held_down = true;
-                if (current_color == 0)
+                isTouchScreenHeld = true;
+                if (activeColorIndex == 0)
                 {
                     WHBLogConsoleSetColor(0x99000000);
-                    current_color = 1;
+                    activeColorIndex = 1;
                 }
-                else if (current_color == 1)
+                else if (activeColorIndex == 1)
                 {
                     WHBLogConsoleSetColor(0x00990000);
-                    current_color = 2;
+                    activeColorIndex = 2;
                 }
-                else if (current_color == 2)
+                else if (activeColorIndex == 2)
                 {
                     WHBLogConsoleSetColor(0x00009900);
-                    current_color = 3;
+                    activeColorIndex = 3;
                 }
-                else if (current_color == 3)
+                else if (activeColorIndex == 3)
                 {
                     WHBLogConsoleSetColor(0x00000000);
-                    current_color = 0;
+                    activeColorIndex = 0;
                 }
             }
         }
         else
         {
-            touch_held_down = false;
+            isTouchScreenHeld = false;
         }
 
         if (input.trigger & VPAD_BUTTON_A)
         {
             WHBLogPrint("You pressed the A button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_B)
         {
             WHBLogPrint("You pressed the B button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_X)
         {
             WHBLogPrint("You pressed the X button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_Y)
         {
             WHBLogPrint("You pressed the Y button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_PLUS)
         {
             WHBLogPrint("You pressed the Start button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_MINUS)
         {
             WHBLogPrint("You pressed the Select button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_L)
         {
             WHBLogPrint("You pressed the L button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_R)
         {
             WHBLogPrint("You pressed the R button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_ZL)
         {
             WHBLogPrint("You pressed the ZL button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_ZR)
         {
             WHBLogPrint("You pressed the ZR button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_STICK_L)
         {
             WHBLogPrint("You pressed the Left Control Stick!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_STICK_R)
         {
             WHBLogPrint("You pressed the Right Control Stick!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_UP)
         {
             WHBLogPrint("You pressed the Up D-Pad button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_DOWN)
         {
             WHBLogPrint("You pressed the Down D-Pad button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_LEFT)
         {
             WHBLogPrint("You pressed the Left D-Pad button!");
-            count();
+            updateButtonPressCount();
         }
         if (input.trigger & VPAD_BUTTON_RIGHT)
         {
             WHBLogPrint("You pressed the Right D-Pad button!");
-            count();
+            updateButtonPressCount();
         }
 
         if (input.trigger & VPAD_BUTTON_SYNC)
